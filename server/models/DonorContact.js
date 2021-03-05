e75dce763d18c989a2f357b34841287f4efaf918
Dohd <alloicebrian@gmail.com>
2021-03-05 19:40:56 +0300

    Create DonorContact model

---
+++ server/models/DonorContact.js
@@ -0,0 +1,14 @@
+const { db, DataTypes } = require('../utils/database');
+
+const DonorContact = db.define('contact_person', {
+    firstName: { type: DataTypes.STRING, allowNull: false },
+    lastName: { type: DataTypes.STRING, allowNull: false },
+    telephone: { type: DataTypes.STRING(15), allowNull: false },
+    email: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+        validate: { isEmail: true } 
+    }
+});
+
+module.exports = DonorContact;
