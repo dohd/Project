4f43a607e0c591795138cb71aedb8626ad329518
Dohd <alloicebrian@gmail.com>
2021-03-05 20:02:59 +0300

    Create DonorContact association

--- server/models/Donor.js
+++ server/models/Donor.js
@@ -1,5 +1,6 @@
 const { db, DataTypes } = require('../utils/database');
 const Proposal = require('./Proposal');
+const DonorContact = require('./DonorContact');
 
 const Donor = db.define('donor', {
     name: { type: DataTypes.STRING, allowNull: false },
@@ -12,6 +13,14 @@ const Donor = db.define('donor', {
 });
 
 module.exports = Donor;
+
+// One-to-One Association
+Donor.hasOne(DonorContact, {
+    foreignKey: { name: 'donorId', allowNull: false },
+    as: 'donorContact',
+    onDelete: 'set null'
+});
+DonorContact.belongsTo(Donor, { as: 'donor' });
 
 // One-to-Many Association
 Donor.hasMany(Proposal, {
