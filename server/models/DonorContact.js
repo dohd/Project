7fade3794ee7052f0924e2c0feb55a0a80041bb4
Dohd <alloicebrian@gmail.com>
2021-03-05 21:07:56 +0300

    Add accountId attribute

--- server/models/DonorContact.js
+++ server/models/DonorContact.js
@@ -1,6 +1,7 @@
 const { db, DataTypes } = require('../utils/database');
 
 const DonorContact = db.define('contact_person', {
+    accountId: { type: DataTypes.INTEGER, allowNull: false  },
     firstName: { type: DataTypes.STRING, allowNull: false },
     lastName: { type: DataTypes.STRING, allowNull: false },
     telephone: { type: DataTypes.STRING(15), allowNull: false },
