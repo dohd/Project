a7b61725569d7491f30f0fd58e7a14e72cdde6f3
Dohd <alloicebrian@gmail.com>
2021-03-14 13:32:38 +0300

    Update activityPlans view

--- client/src/pages/plan/ActivityPlans.jsx
+++ client/src/pages/plan/ActivityPlans.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import { Card, Space, Table } from 'antd';
+import { Card, Space, Table, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+const sampleData = [
+    {
+        key: 1,
+        plan: 'Incididunt voluptate esse aliquip.'
+    },
+    {
+        key: 2,
+        plan: 'Reprehenderit veniam do occaecat'
+    }
+];
 
 export default function ActivityPlans(params) {
     return (
         <Card
+            bordered={false}
             title={
                 <Space>
                     Activity Plans
                 </Space>
             }
+            extra={
+                <Button type='primary'>
+                    <PlusOutlined /> Plan
+                </Button>
+            }
         >
             <Table
+                dataSource={sampleData}
                 columns={[
                     {
                         title: 'Plan',
-                        dataIndex: 'Plan',
+                        dataIndex: 'plan',
                         key: 'plan'
                     },
                     {
                         title: 'Action',
-                        dataIndex: 'action',
-                        key: 'action'
+                        key: 'action',
+                        render: (txt, {key}) => {
+                            return (
+                                <Button type='link'>
+                                    Participants
+                                </Button>
+                            );
+                        }
                     }
                 ]}
             />
