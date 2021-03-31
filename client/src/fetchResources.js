777bd5d5f0197f4e610e0771316441c163df4963
Dohd <alloicebrian@gmail.com>
2021-03-31 13:58:17 +0300

    Create fetchResources

---
+++ client/src/fetchResources.js
@@ -0,0 +1,29 @@
+import Api from 'api';
+
+// Api endpoint name mapping to action type
+const actionTypeMap = {
+    avatarImage: 'addAvatarImage',
+    eventImage: 'addEventImages',
+    orgProfile: 'addOrgProfile',
+    targetGroup: 'addTargetGroups',
+    targetRegion: 'addTargetRegions',
+    keyProgramme: 'addKeyProgrammes',
+    donor: 'addDonors',
+    donorContact: 'addDonorContacts',
+    participant: 'addParticipants',
+    proposal: 'addProposals',
+    agenda: 'addAgenda',
+    activityPlan: 'addActivityPlans',
+    quiz: 'addQuiz',
+    narrative: 'addNarratives',
+    caseStudy: 'addCaseStudies',
+    user: 'addUsers',
+    role: 'addRoles',
+};
+
+export const fetchResources = dispatch => {
+    const actionTypeKeys = Object.keys(actionTypeMap);
+    actionTypeKeys.forEach(key => {
+        Api[key].get().then(res => dispatch({ type: actionTypeMap[key], payload: res}));
+    });
+};
