34ed2e214524166e3b55443c42be32aaa98063dd
Dohd <alloicebrian@gmail.com>
2021-03-31 21:39:59 +0300

    Rename fName and lName

--- server/models/ContactPerson.js
+++ server/models/ContactPerson.js
@@ -1,8 +1,8 @@
 const { db, DataTypes } = require('../utils/database');
 
 const ContactPerson = db.define('contact_person', {
-    firstName: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
+    fName: { type: DataTypes.STRING, allowNull: false },
+    lName: { type: DataTypes.STRING, allowNull: false },
     telephone: { type: DataTypes.STRING(15), allowNull: false },
     email: { 
         type: DataTypes.STRING, 
