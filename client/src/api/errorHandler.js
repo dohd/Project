d7c2fd1fa279fd85238109a29bfa38d82a62b2c3
Dohd <alloicebrian@gmail.com>
2021-04-01 13:15:02 +0300

    Move rendercount below if_block

--- client/src/api/errorHandler.js
+++ client/src/api/errorHandler.js
@@ -2,11 +2,11 @@ import { message } from 'antd';
 
 let renderCount = 0;
 const errorHandler = data => {
-    renderCount++;
     console.log(data);
-    if (renderCount === 2 && data.error) {
+    if (renderCount === 1 && data.error) {
         message.error(data.error.message);
     }
+    renderCount++;
     return Promise.resolve();
 };
 
