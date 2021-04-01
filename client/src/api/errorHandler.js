f682add2471e3048f800230eab88aa3388ecda25
Dohd <alloicebrian@gmail.com>
2021-04-01 14:41:25 +0300

    Remove renderCounter

--- client/src/api/errorHandler.js
+++ client/src/api/errorHandler.js
@@ -1,12 +1,8 @@
 import { message } from 'antd';
 
-let renderCount = 0;
 const errorHandler = data => {
     console.log(data);
-    if (renderCount === 1 && data.error) {
-        message.error(data.error.message);
-    }
-    renderCount++;
+    if (data.error) message.error(data.error.message);
     return Promise.resolve();
 };
 
