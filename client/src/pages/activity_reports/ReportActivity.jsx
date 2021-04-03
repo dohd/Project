1c93b5b6fe7c33c43a4e023695c02064e61e4a06
Dohd <alloicebrian@gmail.com>
2021-04-03 23:09:40 +0300

    Update ReportActivity

--- client/src/pages/activity_reports/ReportActivity.jsx
+++ client/src/pages/activity_reports/ReportActivity.jsx
@@ -1,25 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Card, Table, Button } from 'antd';
-import UrlPattern from 'url-pattern';
-import { Path } from 'routes';
+
 import ReportModal from './ReportModal';
-import { useNarrativeContext } from 'contexts';
 
-export default function ReportActivity({ history }) {
-    const [activities, setActivities] = useState([]);
-    const { narratives } = useNarrativeContext();
-    useEffect(() => { 
-        const json_activities = narratives.map(val => JSON.stringify(val.activity));
-        const unique_activities = Array.from(new Set(json_activities));
-        const activityList = unique_activities.map(val => JSON.parse(val));        
-        setActivities(activityList);
-    }, [narratives]);
-
-    const viewTable = key => {
-        const pattern = new UrlPattern(Path.reportView());
-        const path = pattern.stringify({ activityId: key });
-        history.push(path);
-    };
+export default function ReportActivity({ history }) {    
 
     const [visible, setVisible] = useState(false);
     const [record, setRecord] = useState({});
@@ -36,12 +20,11 @@ export default function ReportActivity({ history }) {
             <ReportModal 
                 visible={visible}
                 setVisible={setVisible}
-                viewTable={viewTable}
                 record={record}
             />
 
             <Table 
-                dataSource={activities}
+                // dataSource={activities}
                 columns={[
                     {
                         title: 'Activity',
--- client/src/pages/activity_reports/index.js
+++ client/src/pages/activity_reports/index.js
@@ -1,3 +1,2 @@
 export { default as ReportActivity } from './ReportActivity';
-// export { default as ReportView } from './ReportView';
 export { default as ReportTable } from './ReportTable';
