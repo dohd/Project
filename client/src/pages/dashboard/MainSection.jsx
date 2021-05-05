e103959e44ce09e94f105d816063526f28a99193
Dohd <alloicebrian@gmail.com>
2021-05-06 00:21:24 +0300

    Update rendered components

--- client/src/pages/dashboard/MainSection.jsx
+++ client/src/pages/dashboard/MainSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from 'react-router-dom';
 
 import { Path } from 'routes';
+
 import { CreateProposal, Proposals } from '../grant_proposal';
 import { EditPendingProposal } from '../pending_proposal';
 import { 
@@ -11,12 +12,10 @@ import {
 } from '../participant';
 import { ParticipantAnalysis } from '../participant_analysis';
 // import { Implement, PendingReport } from '../pending_action';
-import { ActivityPhoto, EventActivity } from '../event_photo';
-import { CaseStudy } from '../case_study';
 import { TargetGroups } from '../target_group';
 import { KeyProgrammes } from '../key_programme';
 import { Regions } from '../target_region';
-import { EventPlans } from '../event';
+import { EventPlan } from '../event';
 import { ActivityPlans  } from '../plan';
 import { Narrative } from '../narative_report';
 import { Agenda } from '../agenda';
@@ -28,7 +27,10 @@ import { Settings } from '../setting';
 import { Home } from '../home';
 import { Objectives, Activities } from './ObjActWrapper';
 import { Bargraph } from '../graphs';
-import { ReportActivity, ReportTable } from '../activity_reports';
+import { ActivityReport } from '../activity_report';
+import { CaseStudy } from '../case_study';
+import {  ReportImage } from '../report_image';
+import { ReportResponse } from '../report_response';
 
 export default function MainSection() {
     // reset scrollbar position
@@ -37,17 +39,17 @@ export default function MainSection() {
     return (
         <>
             <Switch>
+                <Route exact path={Path.reportResponses()} component={ReportResponse} />
+
+                <Route exact path={Path.reportImages()} component={ReportImage} />
+
                 <Route exact path={Path.settings()} component={Settings} />
                 <Route exact path={Path.graphs()} component={Bargraph} />
                 
-                <Route exact path={Path.reportView()} component={ReportTable} />
                 <Route exact path={Path.home()} component={Home} />
 
                 <Route exact path={Path.caseStudies()} component={CaseStudy} />
                 <Route exact path={Path.users()} component={Users} />
-
-                <Route exact path={Path.eventPhotos()} component={EventActivity} />
-                <Route exact path={Path.activityPhoto()} component={ActivityPhoto} />
 
                 <Route exact path={Path.objectives()} component={Objectives} />
                 <Route exact path={Path.proposals()} component={Proposals} />
@@ -64,7 +66,7 @@ export default function MainSection() {
 
                 <Route exact path={Path.updateParticipant()} component={UpdateParticipant} />
 
-                <Route exact path={Path.eventPlans()} component={EventPlans} />
+                <Route exact path={Path.eventCalendar()} component={EventPlan} />
                 <Route exact path={Path.participantAnalysis()} component={ParticipantAnalysis} />
 
                 <Route exact path={Path.programmes()} component={KeyProgrammes} />
@@ -73,8 +75,8 @@ export default function MainSection() {
                 <Route exact path={Path.regions()} component={Regions} />
                 <Route exact path={Path.activityPlans()} component={ActivityPlans} />
 
-                {/* <Route exact path={Path.responses()} component={Responses} /> */}
-                <Route exact path={Path.reportActivity()} component={ReportActivity} />
+                
+                <Route exact path={Path.activityReport()} component={ActivityReport} />
 
                 <Route exact path={Path.activities()} component={Activities} />
                 {/* <Route exact path={Path.implement()} component={Implement} /> */}
