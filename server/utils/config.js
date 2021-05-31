24aa83cb9eec851e86306d6a7d12dfcfeacebfe8
Dohd <alloicebrian@gmail.com>
2021-05-31 13:41:15 +0300

    Configure static server

--- server/utils/config.js
+++ server/utils/config.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 const app = express();
 const corsOptions = {
     origin: [
-        'http://localhost:5000', 
-        'http://127.0.0.1:5000'
+        'http://localhost:3000', 
+        'http://127.0.0.1:3000'
     ],
     maxAge: 86400,
     credentials: true
