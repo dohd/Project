39e59f859b7d7cf31a9136c8ade912969fea55ad
Dohd <alloicebrian@gmail.com>
2021-06-06 23:53:18 +0300

    Suffix REACT_APP

--- client/src/utils/firebaseConfig.js
+++ client/src/utils/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "proposal-app-5d4b6.firebaseapp.com",
     projectId: "proposal-app-5d4b6",
     storageBucket: "proposal-app-5d4b6.appspot.com",
-    messagingSenderId: process.env.MESSAGE_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
